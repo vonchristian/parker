5224590b5cd231ba6ae0e60513b035f30acb73c6
Von Christian <vc.halip@gmail.com>
2020-08-18 07:28:39 +0800

    Default to true for new slots

--- slot.rb
+++ slot.rb
@@ -2,7 +2,7 @@ class Slot
   attr_accessor :availability_status
 
   def initialize(args={})
-    @availability_status = args[:availability_status]
+    @availability_status = args[:availability_status] || true
   end
 
   def available?

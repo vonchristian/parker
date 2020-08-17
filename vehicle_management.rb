7d19d72dbfee6481909a0cbf76e3759eaa51423e
Von Christian <vc.halip@gmail.com>
2020-08-18 07:21:11 +0800

    Add querying of vehicles with certain color

--- vehicle_management.rb
+++ vehicle_management.rb
@@ -4,4 +4,11 @@ class VehicleManagement
   def initialize(args={})
     @vehicles = []
   end
+
+  def plate_number_for_vehicles_with_color(color)
+    plate_numbers = []
+
+    vehicles.select{ |v| v.color == color }.each { |vehicle| plate_numbers << vehicle.plate_number }
+    puts plate_numbers.join(',')
+  end
 end

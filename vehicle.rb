09cb0f39dc36bdad12fd3f24f3f32ad566e565c0
Von Christian <vc.halip@gmail.com>
2020-08-18 06:57:44 +0800

    Generate plate number when none given

--- vehicle.rb
+++ vehicle.rb
@@ -3,8 +3,15 @@ class Vehicle
   attr_accessor :plate_number, :parking_status, :color
 
   def initialize(args={})
-    @plate_number   = args[:plate_number]
+    @plate_number   = args[:plate_number] || generate_plate_number
     @parking_status = args[:parking_status]
     @color          = args[:color]
+  end
+
+  def generate_plate_number
+    strings = ("A".."Z").to_a.sample(3).join
+    numbers = (1..9).to_a.sample(4).join
+
+    strings + "-" + numbers
   end
 end

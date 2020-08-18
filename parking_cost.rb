cea802ce14c7c5021b417d853a3eccb21f519b5d
Von Christian <vc.halip@gmail.com>
2020-08-18 13:03:14 +0800

    Pass grace period when computing parking cost

--- parking_cost.rb
+++ parking_cost.rb
@@ -1,11 +1,16 @@
 class ParkingCost
-  attr_reader :hourly_rate, :duration
+  attr_reader :hourly_rate, :duration, :grace_period_in_minutes
 
   def initialize(args={})
     @hourly_rate = args.fetch(:hourly_rate)
     @duration    = args.fetch(:duration)
+    @grace_period_in_minutes = args.fetch(:grace_period_in_minutes)
   end
   def amount
-    duration * hourly_rate
+    duration_less_grace_period * hourly_rate
+  end
+
+  def duration_less_grace_period
+    duration - (grace_period_in_minutes / 60.0) #60 mins per hour
   end
 end

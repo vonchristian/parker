7a680e9d34938fbbb5bb9effa13d97f0a7bb3d8f
Von Christian <vc.halip@gmail.com>
2020-08-18 13:18:08 +0800

    Fix duration computation when vehicle is still parked

--- parking.rb
+++ parking.rb
@@ -1,12 +1,13 @@
 require 'time'
 class Parking
-  attr_accessor :vehicle, :slot, :entry_time, :departure_time
+  attr_reader   :vehicle, :slot
+  attr_accessor :entry_time, :departure_time
 
   def initialize(args={})
     @vehicle        = args.fetch(:vehicle)
     @slot           = args.fetch(:slot)
     @entry_time     = args[:entry_time] || generate_entry_time
-    @departure_time = args[:departure_time] || generate_departure_time
+    @departure_time = args[:departure_time]
   end
 
   def park!
@@ -17,7 +18,7 @@ class Parking
   def unpark!(departure_time)
     vehicle.parking_status = false
     slot.availability_status = true
-    departure_time = departure_time
+    self.departure_time = departure_time
   end
 
   def generate_entry_time
@@ -29,9 +30,13 @@ class Parking
   end
 
   def duration_in_hours
-    entry     = Time.parse(entry_time.to_s)
-    departure = Time.parse(departure_time.to_s)
+    if !vehicle.parked?
+      entry     = Time.parse(entry_time.to_s)
+      departure = Time.parse(departure_time.to_s)
 
-    ((departure - entry) / 3600).round(2)
+      ((departure - entry) / 3600).round(2)
+    else
+      0
+    end
   end
 end

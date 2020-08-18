6e8b0e3941a9f58931261d3717ad10781d7ee390
Von Christian <vc.halip@gmail.com>
2020-08-18 13:04:02 +0800

    Fix computing of parking cost

--- parking_management.rb
+++ parking_management.rb
@@ -44,10 +44,11 @@ class ParkingManagement
     slot_number = parking_lot.slots.find_index(parking.slot) +1
 
     parking_lot.unpark(parking.vehicle, departure_time)
-    amount = ParkingCost.new(duration: parking.duration_in_hours, hourly_rate: parking_lot.hourly_rate).amount
-
+    amount = ParkingCost.new(duration: parking.duration_in_hours, hourly_rate: parking_lot.hourly_rate, grace_period_in_minutes: parking_lot.grace_period_in_minutes).amount
+    ticket = parking_lot.issued_tickets.select{|ticket| ticket.parking == parking }.first
+    ticket.price = amount
     puts "Vehicle with plate number #{plate_number} is now leaving. Departure time: #{departure_time}."
-    puts "Duration: #{parking.duration_in_hours} hour/s: Paid: P#{amount}"
+    puts "Duration: #{parking.duration_in_hours} hour/s: Paid: P#{amount.round(2)}"
     puts"Slot #{slot_number} is now free"
   end
 end

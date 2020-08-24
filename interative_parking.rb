59b91c62a3e3dbf5d0aa3180b8052ec58926c575
Von Christian <vc.halip@gmail.com>
2020-08-24 22:12:53 +0800

    Add command for create_parking_lot()

--- interative_parking.rb
+++ interative_parking.rb
@@ -10,3 +10,21 @@ puts 'create_parking_lot(slots, hourly_rate, grace_period)'
 puts 'park(plate_number, vehicle_color, entry_time)'
 puts "\n"
 
+loop do
+  input = gets.chomp
+  command, *params = input.split /\s/
+
+  case command
+  when /\Acreate_parking_lot\z/i
+    number_of_slots         = params[0].to_i
+    hourly_rate             = params[1].to_f
+    grace_period_in_minutes = params[2].to_f
+
+    parking_manager.create_parking_lot(
+      number_of_slots:         number_of_slots,
+      hourly_rate:             hourly_rate,
+      grace_period_in_minutes: grace_period_in_minutes)
+      puts "\n"
+
+
+end
